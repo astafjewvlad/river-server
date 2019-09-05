406e510b709ef8d2bde548ec0ec1d5438bb5e5f6
astafjew vlad <astafjewvlad@gmail.com>
2019-09-06 02:40:59 +0300

    static files optimized

--- index.js
+++ index.js
@@ -12,15 +12,11 @@ const byRoot = (filepath) => {
   return path.join(__dirname, filepath);
 }
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   const byTemplates = (filepath) => path.join(byRoot('/templates'), filepath);
   res.sendFile(byTemplates('/index.html'));
-});
-
-app.get('/public/:filedir/:filepath', (req, res) => {
-  const {filedir, filepath} = req.params;
-  const byPublic = (filepath) => path.join(byRoot('/public'), filepath);
-  res.sendFile(byPublic(path.join(filedir, filepath)));
 });
 
 app.listen(server.port, server.host, ()=> {
