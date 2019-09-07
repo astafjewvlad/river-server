7329d0d1a5f45cd2ed52c8406e8953262416614e
astafjew vlad <astafjewvlad@gmail.com>
2019-09-07 15:44:17 +0300

    added tracks json repos

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
@@ -17,6 +18,17 @@ app.set('views', './templates');
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/tracktest', (req, res) => {
+  fs.readFile(byRoot('/repos/tracks.json'), (err, data) => {
+    if (err) {
+      res.sendStatus(404);
+      return;
+    }
+    const { tracks } = JSON.parse(data);
+    res.render('tracktest', { tracks });
+  });
 });
 
 app.listen(server.port, server.host, () => {
